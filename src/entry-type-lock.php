235af574cf6c7575bc4726f2b0051f9642c6fc09
Michael Van Dorth <michael@fostercommerce.com>
2021-08-04 03:35:45 +0200

    Add sample config file

---
+++ src/entry-type-lock.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'sections' => [
+        /*
+        // Add each section you want to lock down entry types for
+        'pages' => [
+            // Add each entry type in the section you want to lock (by limit and/or user group)
+            'contact' => [
+                'limit' => 1,
+                'userGroups' => [
+                    'adminGroup1',
+                    'adminGroup2'
+                ]
+            ],
+            'blog' => [
+                'limit' => 1
+            ]
+        ]
+        */
+    ]
+];
