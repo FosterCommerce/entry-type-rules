f0f87ca90b2b95c374497a286212b20418635f69
Michael Van Dorth <michael@fostercommerce.com>
2022-02-18 12:28:03 +0100

    Translations

--- src/translations/en/entry-type-lock.php
+++ src/translations/en/entry-type-lock.php
@@ -3,7 +3,7 @@
  * Entry Type Lock plugin for Craft CMS 3.x
  *
  * A Craft plugin that allows you to lock down the number of entry types in a Craft section and/or limit who can
-include entry types based on their user group.
+ * include entry types based on their user group.
  *
  * @link      https://fostercommerce.com
  * @copyright Copyright (c) 2022 Foster Commerce
@@ -22,5 +22,20 @@ include entry types based on their user group.
  * @since     1.0.0
  */
 return [
-    'Entry Type Lock plugin loaded' => 'Entry Type Lock plugin loaded',
+    'Entry Type Lock' => 'Entry Type Lock',
+    'Edit Plugin Settings' => 'Edit Plugin Settings',
+    '{name} plugin loaded' => '{name} plugin loaded',
+    'Settings' => 'Settings',
+    'Warning' => 'Warning',
+    'The Entry Type Lock plugin settings are being overridden in the \'config/entry-type-lock.php\' file.' => 'The Entry Type Lock plugin settings are being overridden in the \'config/entry-type-lock.php\' file.',
+    'Section' => 'Section',
+    'Entry Type' => 'Entry Type',
+    'The entry type and its current total entry count.' => 'The entry type and its current total entry count.',
+    'Limit' => 'Limit',
+    'Limit the number of entries for the entry type.' => 'Limit the number of entries for the entry type.',
+    'Leave blank or set to zero to remove limits for the entry type.' => 'Leave blank or set to zero to disable limits for the entry type.',
+    'User Groups' => 'User Groups',
+    'Limit which user groups can add new entries for the entry type.' => 'Limit which user groups can add new entries for the entry type.',
+    'Admin users will always be able to add new entries.' => 'Admin users will always be able to add new entries.',
+    'Entries' => 'Entries'
 ];
