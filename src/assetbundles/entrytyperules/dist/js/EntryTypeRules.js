48868f072ffa31b87e5325f75598dbda1ee502ec
Pete Eveleigh <pete@fostercommerce.com>
2024-09-23 22:47:03 +0100

    rework js check for disabling entry type buttons

--- src/assetbundles/entrytyperules/dist/js/EntryTypeRules.js
+++ src/assetbundles/entrytyperules/dist/js/EntryTypeRules.js
@@ -31,12 +31,12 @@
 			if (self.namespace) {
 				self.sectionId = $('#' + self.namespace + '-entryTypeRulesSectionId').data('value');
 				self.$typeSelector = $('#' + self.namespace + '-entryType');
-				self.$typeButton = $('#' + self.namespace + '-entryType-button');
+				self.$typeButton = $('#' + self.namespace + '-entryType-button').data('menubtn');
 				self.$typeInput = $('#' + self.namespace + '-entryType-input');
 			} else {
 				self.sectionId = $('#entryTypeRulesSectionId').data('value');
 				self.$typeSelector = $('#entryType');
-				self.$typeButton = $('#' + 'entryType-button');
+				self.$typeButton = $('#' + 'entryType-button').data('menubtn');
 				self.$typeInput = $('#' + 'entryType-input');
 			}
 
@@ -57,32 +57,13 @@
 					dataType: "json"
 				}).done(function (response) {
 					if (response.lockedEntryTypes) {
-						//if the currently selected entry type change it to the first non-disabled
-						response.lockedEntryTypes.forEach( (lockedEntryType) => {
-							if(self.$typeInput.val() == lockedEntryType) {
-								self.$typeButton.find('.label:first').addClass('disabled');
-								// set the button and the hidden input to the first non-disabled entry type
-								// but we don't know what they are until the button is clicked...hmmm
+					
+						jQuery(self.$typeButton.menu.$options).each( ($option) => {
+							if(jQuery.inArray(jQuery(self.$typeButton.menu.$options[$option]).data().value, response.lockedEntryTypes) !== -1) {
+								jQuery(self.$typeButton.menu.$options[$option]).disabled = true
+								jQuery(self.$typeButton.menu.$options[$option]).addClass('disabled')
 							}
 						})
-
-						// Disable the locked entry types in the 'select'
-						// doing this with vanilla JS because I can't get the jQuery selector to work even if I escape the . in the menu's ID.
-						self.$typeSelector.on('click', function(ev){
-							const listBoxId = ev.currentTarget.querySelector('button').getAttribute('aria-controls')
-							const listBox = document.getElementById(listBoxId)
-							const buttons = listBox.querySelectorAll('.menu-item');
-							buttons.forEach( (button) => {
-								response.lockedEntryTypes.forEach( (lockedEntryType) => {
-									if(lockedEntryType == button.dataset.value){
-										button.disabled = true
-										button.classList.remove('sel')
-										button.classList.add('disabled')
-									}
-								})
-							})
-						})
-						
 
 						// Find out if we are on the new entry (URL param "fresh" is present)
 						const urlParams = new URLSearchParams(window.location.search);
